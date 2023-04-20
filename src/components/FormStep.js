d29c91046ea1f4559a3dbdf441b8c94d68c8b783
Rishav Bharti <rishav.bharti@omnycomm.com>
2023-04-21 00:09:23 +0530

    add multistep form

---
+++ src/components/FormStep.js
@@ -0,0 +1,68 @@
+import Form1 from './Form1';
+import Form2 from './Form2';
+import Form3 from './Form3';
+
+const FormStep = ({ step, handleChange, formData, nextStep, prevStep, handleSubmit, setStep }) => {
+  const renderForm = () => {
+    switch (step) {
+      case 1:
+        return <Form1 handleChange={handleChange} formData={formData} nextStep={nextStep} />;
+      case 2:
+        return (
+          <Form2
+            handleChange={handleChange}
+            formData={formData}
+            nextStep={nextStep}
+            prevStep={prevStep}
+          />
+        );
+      case 3:
+        return (
+          <Form3
+            handleChange={handleChange}
+            formData={formData}
+            prevStep={prevStep}
+            handleSubmit={handleSubmit}
+          />
+        );
+      default:
+        return <div>Invalid step</div>;
+    }
+  };
+
+  const tabButtonClasses = 'btn btn-link text-decoration-none';
+
+  return (
+    <div>
+      <div className="btn-group" role="group">
+        <button
+          type="button"
+          className={`${tabButtonClasses} ${step === 1 ? 'fw-bold' : ''}`}
+          disabled={step === 1}
+          onClick={() => setStep(1)}
+        >
+          Form 1
+        </button>
+        <button
+          type="button"
+          className={`${tabButtonClasses} ${step === 2 ? 'fw-bold' : ''}`}
+          disabled={step === 2}
+          onClick={() => setStep(2)}
+        >
+          Form 2
+        </button>
+        <button
+          type="button"
+          className={`${tabButtonClasses} ${step === 3 ? 'fw-bold' : ''}`}
+          disabled={step === 3}
+          onClick={() => setStep(3)}
+        >
+          Form 3
+        </button>
+      </div>
+      {renderForm()}
+    </div>
+  );
+};
+
+export default FormStep;
---
+++ src/components/MultiStepForm.js
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import FormStep from './FormStep';
+
+const MultiStepForm = () => {
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState({});
+
+  const navigate = useNavigate();
+
+  const nextStep = () => {
+    setStep(step + 1);
+  };
+
+  const prevStep = () => {
+    setStep(step - 1);
+  };
+
+  const handleChange = (name, value) => {
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('https://codebuddy.review/submit', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        navigate('/posts');
+      } else {
+        throw new Error('Failed to submit');
+      }
+    } catch (error) {
+      throw new Error('Failed to submit');
+    }
+  };
+
+  return (
+    <div className="container mt-5">
+      <FormStep
+        step={step}
+        handleChange={handleChange}
+        formData={formData}
+        nextStep={nextStep}
+        prevStep={prevStep}
+        setStep={setStep}
+        handleSubmit={handleSubmit}
+      />
+    </div>
+  );
+};
+
+export default MultiStepForm;
