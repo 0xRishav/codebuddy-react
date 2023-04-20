f0fd1ed37fab4616d3ab44e3c0e4ed5cc5759305
Rishav Bharti <rishav.bharti@omnycomm.com>
2023-04-21 00:09:34 +0530

    add form to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,15 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import { useNavigate } from 'react-router-dom';
+import MultiStepForm from '../components/MultiStepForm';
 
-const Home = () => {
-  const navigate = useNavigate();
-
-  const onSubmit = () => navigate('/posts');
-
-  return (
-    <main>
-      <div className="bg-light p-5 mb-5">
-        <h1>React + Bootstrap v4</h1>
-        <p>React template with Bootstrap version v4</p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
-      </div>
-      <Container>
-        <Form>
-          <Button onClick={onSubmit}>Goto Posts</Button>
-        </Form>
-      </Container>
-    </main>
-  );
-};
+const Home = () => (
+  <main>
+    <div className="bg-light p-5 mb-5">
+      <h1>CodeBuddy Assignment</h1>
+    </div>
+    <Container>
+      <MultiStepForm />
+    </Container>
+  </main>
+);
 
 export default Home;
